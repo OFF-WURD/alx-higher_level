edd6cf82c1e4adbae19c3e6c5d57b3abba444385
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-03-17 23:23:40 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     argc = len(argv)
     if argc < 2:
         print("{} arguments.".format(argc - 1))
-    else:
-    if argc == 2:
+    elif argc == 2:
         print("{} argument:".format(argc - 1))
     else:
         print("{} arguments:".format(argc - 1))
